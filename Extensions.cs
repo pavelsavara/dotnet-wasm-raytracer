c52c92270f248778d0d44ddfaf0554dcec50e286
Radek Doulik <radekdoulik@gmail.com>
2022-09-05 13:18:52 +0200

    Move extensions to separate file

---
+++ Extensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.Intrinsics;
+
+namespace RayTracer
+{
+    public static class Extensions
+    {
+        static public float Magnitude(this Vector128<float> v)
+        {
+            return (float)Math.Sqrt(Vector128.Dot(v, v));
+        }
+
+        static public Vector128<float> Normalize(this Vector128<float> v)
+        {
+            return v / Vector128.Create(v.Magnitude());
+        }
+
+        static public float X(this Vector128<float> v)
+        {
+            return v.GetElement(0);
+        }
+        static public float Y(this Vector128<float> v)
+        {
+            return v.GetElement(1);
+        }
+        static public float Z(this Vector128<float> v)
+        {
+            return v.GetElement(2);
+        }
+    }
+}
--- Ray.cs
+++ Ray.cs
@@ -20,30 +20,4 @@ namespace RayTracer
         public Ray(Vector128<float> start, Vector128<float> direction) : this(start, direction, float.PositiveInfinity) { }
 
     }
-
-    public static class Extensions
-    {
-        static public float Magnitude(this Vector128<float> v)
-        {
-            return (float)Math.Sqrt(Vector128.Dot(v, v));
-        }
-
-        static public Vector128<float> Normalize(this Vector128<float> v)
-        {
-            return v / Vector128.Create(v.Magnitude());
-        }
-
-        static public float X(this Vector128<float> v)
-        {
-            return v.GetElement(0);
-        }
-        static public float Y(this Vector128<float> v)
-        {
-            return v.GetElement(1);
-        }
-        static public float Z(this Vector128<float> v)
-        {
-            return v.GetElement(2);
-        }
-    }
 }
