464bfa1aa400d2756d25f7a598e6ad616b7aa0f9
pavelsavara <pavel.savara@gmail.com>
2024-05-14 13:51:48 +0200

    net9 preview 3 MT

--- main.js
+++ main.js
@@ -1,24 +1,57 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-import { dotnet } from './dotnet.js'
+import { dotnet } from './_framework/dotnet.js' 
+
+function drawWaitingForRendering(canvas) {
+    const ctx = canvas.getContext('2d');
+    ctx.save();
+    ctx.fillStyle = "rgba(255,255,255,0.7)";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.restore();
+    ctx.save();
+    ctx.font = "bold 50px Arial, sans-serif";
+    ctx.fillStyle = "darkred";
+    ctx.textAlign = "center";
+    ctx.fillText("Rendering...", canvas.width / 2, canvas.height / 2);
+    ctx.restore();
+}
 
 function renderCanvas(rgbaView) {
-    const canvas = document.getElementById("out");
     const ctx = canvas.getContext('2d');
     const clamped = new Uint8ClampedArray(rgbaView.slice());
-    const image = new ImageData(clamped, 640, 480);
+    const image = new ImageData(clamped, canvas.width, canvas.height);
     ctx.putImageData(image, 0, 0);
-    rgbaView.dispose();
     canvas.style = "";
 }
 
+function setOutText(text) {
+    outText.innerText = text;
+}
+
+async function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+
 const { setModuleImports, getAssemblyExports, getConfig } = await dotnet.create();
-setModuleImports("main.js", { renderCanvas });
+setModuleImports("main.js", { renderCanvas, setOutText });
 const config = getConfig();
 const exports = await getAssemblyExports(config.mainAssemblyName);
-globalThis.onClick = exports.MainJS.OnClick;
-
-await dotnet.run();
+const canvas = document.getElementById("out");
+const outText = document.getElementById("outText");
 const btnRender = document.getElementById("btnRender");
+
+globalThis.onClick = async function () {
+    btnRender.disabled = true;
+    drawWaitingForRendering(canvas);
+    await delay(10);
+    await exports.MainJS.OnClick();
+    btnRender.disabled = false;
+}
+
 btnRender.disabled = false;
+await exports.MainJS.PrepareToRender(canvas.width, canvas.height, globalThis.navigator.hardwareConcurrency);
+
+
